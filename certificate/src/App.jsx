b8ea4ba0c69f1865412cef6216659a4d6d528386
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-08-25 14:14:03 +0530

    path issue

--- certificate/src/App.jsx
+++ certificate/src/App.jsx
@@ -1,6 +1,6 @@
 import Frontend from "./pages/certifications/Frontend";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Mern from "./pages/certifications/Mern";
 import Python from "./pages/certifications/Python";
 import Backend from "./pages/certifications/Backend";
@@ -20,45 +20,49 @@ import ProtectedRoute from "./components/ProtectedRoutes";
 function App() {
   return (
     <div>
-      <UserProvider>
-        <div>
-          <ToastContainer position="top-right" autoClose={3000} />
-          <Routes>
-            
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            {/* <Route path="/certifications" element={<Certifications />} /> */}
-            <Route path="/certifications/frontend" element={<Frontend />} />
-            <Route path="/certifications/fullstack" element={<Fullstack />} />
-            <Route path="/certifications/mern" element={<Mern />} />
-            <Route path="/certifications/backend" element={<Backend />} />
-            <Route path="/certifications/python" element={<Python />} />
-            <Route path="/certifications/aws" element={<Aws />} />
-            {/* login and signup */}
+      <HashRouter>
+        <UserProvider>
+          <div>
+            <ToastContainer position="top-right" autoClose={3000} />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              {/* <Route path="/certifications" element={<Certifications />} /> */}
+              <Route path="/certifications/frontend" element={<Frontend />} />
+              <Route path="/certifications/fullstack" element={<Fullstack />} />
+              <Route path="/certifications/mern" element={<Mern />} />
+              <Route path="/certifications/backend" element={<Backend />} />
+              <Route path="/certifications/python" element={<Python />} />
+              <Route path="/certifications/aws" element={<Aws />} />
+              {/* login and signup */}
 
-            <Route path="/login" element={<Login />} />
-            
-            <Route path="/signup" element={<Signup />} />
-            {/* admin login */}
-            <Route path="/admin-login" element={<AdminLogin />} />
-            {/* Add more routes as needed */}
-            <Route path="/admin-dashboard" element={<AdminDashboard />} />
-            <Route path="/admin/task/:userId" element={<ViewTask />} />
-            <Route path="/user-dashboard" element={<UserDashboard />} />
-            <Route path="/user-dashboard/:userId" element={<UserDashboard />} />
-            <Route
-              path="/certificate-preview"
-              element={<CertificactePreview />}
-            />
-          </Routes>
-        </div>
-      </UserProvider>
+              <Route path="/login" element={<Login />} />
+
+              <Route path="/signup" element={<Signup />} />
+              {/* admin login */}
+              <Route path="/admin-login" element={<AdminLogin />} />
+              {/* Add more routes as needed */}
+              <Route path="/admin-dashboard" element={<AdminDashboard />} />
+              <Route path="/admin/task/:userId" element={<ViewTask />} />
+              <Route path="/user-dashboard" element={<UserDashboard />} />
+              <Route
+                path="/user-dashboard/:userId"
+                element={<UserDashboard />}
+              />
+              <Route
+                path="/certificate-preview"
+                element={<CertificactePreview />}
+              />
+            </Routes>
+          </div>
+        </UserProvider>
+      </HashRouter>
     </div>
   );
 }
