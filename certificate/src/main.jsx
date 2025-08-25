0ea34688105bb38b3ebf5dc3bf6022493b9ad683
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-08-25 14:21:28 +0530

    path issue

--- certificate/src/main.jsx
+++ certificate/src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
+    {/* <BrowserRouter>
+    </BrowserRouter> */}
   </React.StrictMode>
 )
